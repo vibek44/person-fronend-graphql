56998f3a5404df4639857dd7869715878db260dd
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-02 18:37:04 +0300

    importing ApolloProvider and using it to make access client for child component of App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
