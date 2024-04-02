23bb2b10eb714008587ba869437b62e1cf6558dc
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-02 18:53:35 +0300

    adding single quote as per eslint rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
 
 const App = () => {
-  return <div>welcome to person info</div>;
-};
+  const result = useQuery(ALL_PERSONS)
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  return <div>{result.data.allPersons.map((p) => p.name).join(',')}</div>
+}
 
-export default App;
+export default App
