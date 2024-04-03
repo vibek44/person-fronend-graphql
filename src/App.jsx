0b49e29d3309b31d5f5c3cd8ee2ea9a5630e0846
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-03 21:31:04 +0300

    adding notification component and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { gql, useQuery } from '@apollo/client'
+import { useState } from 'react'
+import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`
+import { ALL_PERSONS } from './queries'
+import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
 
 const App = () => {
+  const [errorMessage ,setErrorMessage]=useState(null)
   const result = useQuery(ALL_PERSONS)
+  const handleNotify=(message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    },10000)
+  }
   if (result.loading) {
     return <div>loading...</div>
   }
-  return <div>< Persons persons={result.data.allPersons}/></div>
+  return <div>
+    <Notification errorMessage={errorMessage}/>
+    < Persons persons={result.data.allPersons}/>
+    < PersonForm handleNotify={handleNotify}/>
+  </div>
 }
 
 export default App
