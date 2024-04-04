b283e023a73811f0b394528254fc4177f09e0df1
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-04 18:20:05 +0300

    Importing EditPhoneForm and passing handleNotify as props

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Persons from './components/Persons'
 import { ALL_PERSONS } from './queries'
 import PersonForm from './components/PersonForm'
 import Notification from './components/Notification'
+import EditPhoneForm from './components/EditPhoneForm'
 
 const App = () => {
   const [errorMessage ,setErrorMessage]=useState(null)
@@ -12,7 +13,7 @@ const App = () => {
     setErrorMessage(message)
     setTimeout(() => {
       setErrorMessage(null)
-    },10000)
+    },3000)
   }
   if (result.loading) {
     return <div>loading...</div>
@@ -21,6 +22,7 @@ const App = () => {
     <Notification errorMessage={errorMessage}/>
     < Persons persons={result.data.allPersons}/>
     < PersonForm handleNotify={handleNotify}/>
+    <EditPhoneForm handleNotify={handleNotify}/>
   </div>
 }
 
